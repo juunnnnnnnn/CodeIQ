e31e28b7128342a2be88a04c9a2994a3addec345
juunnnnnnnn <juunnnnnnnn@gmail.com>
2017-11-23 00:37:58 +0900

    2017_1122
    codeIQの問題が解けた時点

--- CodeIQ/src/combination/Combination.java
+++ CodeIQ/src/combination/Combination.java
@@ -1,110 +1,124 @@
 package combination;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 
 public class Combination {
 	public static void main(String[]args){
-		int lattice;
-		lattice=4;
+		int number=10;//標準入力より取得予定
+		int lattice=number*number;//格子の数
+		int finalAnswer=0;
 
-		//座標を生成
+
+		//x,y座標を生成
 		ArrayList<Tuple2<Integer, Integer>> ar= new ArrayList<Tuple2<Integer, Integer>>();
-		for(int i=0; i<lattice; i++){
-			for (int j = 0; j<lattice;j++){
-				Tuple2<Integer, Integer> tuple2=Tuple.of(i, j);
+		for(int ii=0; ii<number; ii++){
+			for (int j = 0; j<number;j++){
+				//@param
+				Tuple2<Integer, Integer> tuple2=Tuple.of(ii, j);
 				ar.add(tuple2);
-				System.out.println("x:"+tuple2.car);
-				System.out.println("y:"+tuple2.cdr.car);
+
+				//試し　あとで消す
+					System.out.println("試しに出力　x:"+tuple2.car);
+					System.out.println("試しに出力　y:"+tuple2.cdr.car);
 
 			}
 		}
-
 		//試し　あとで消す
+		System.out.println("\r\n");
 		System.out.println("arを出力");
-		System.out.println("x:"+ar.get(0).car);
-		System.out.println("y:"+ar.get(0).cdr.car);
-		System.out.println("x:"+ar.get(15).car);
-		System.out.println("y:"+ar.get(15).cdr.car);
+
+		for(int i4 = 0;i4<lattice;i4++){
+			System.out.println("x:"+ar.get(i4).car);
+			System.out.println("y:"+ar.get(i4).cdr.car);
+		}
 
 
 		//組み合わせを計算
-		Combination combination = new Combination();
-		Integer intArray[]= new Integer[(int) Math.pow(lattice,2)];
+		//Combination combination = new Combination();
+		Integer intArray[]= new Integer[lattice];
+
+		Set<Integer> src = new HashSet<Integer>() {};
 
 		for(Integer h= 0; h<intArray.length; h++){
 			intArray[h]=h;
+			src.add(h);
+
 			//あとで消す
 			System.out.println("intArrayの値"+intArray[h]);
 		}
 
+		//一番候補
+    System.out.println(combination2(src, 2)); // 上記集合のなかから2つを取りだした集合を全て列挙する
 
-		//以下①はうまくいった処理
-//		ArrayList<Integer> n1 = new ArrayList<>(Arrays.asList(1,2,3,4));//与えられた数字4として、仮おき
+    //組み合わせごとに処理を実行
+    for(Set<Integer> tmp : combination2(src, 2)){
+    	int countOfQuestion = 0;
+       // System.out.println(tmp);
+        System.out.println("\r\ntmp.toString "+tmp.toString());
+        String result1=tmp.toString().replaceAll("\\[", "");
+        result1=result1.replaceAll("\\]", "");
 
-		ArrayList<Integer> n1 = new ArrayList<>(Arrays.asList(combination.combining(lattice)));//与えられた数字4として、仮おき
+        System.out.println("result1:"+result1);
+        Pattern p = Pattern.compile("[,\\s]+");
+        String[] result2=p.split(result1);
+        for(int i3 = 0; i3<result2.length;i3++){
+        	System.out.println("正規表現で分割した値："+Integer.parseInt(result2[i3]));//result2[0]が、1つめの座標。result2[1]が、2つめの座標。
+        }
 
-		combination.getCombination(n1,2);
-	}
+        	//座標毎に処理を実行する。
+        	for(int i5=0;i5<lattice;i5++){
+            	System.out.println("linearEquation:"+
+        		linearEquation(
+            			ar.get(Integer.parseInt(result2[0])).car, ar.get(Integer.parseInt(result2[0])).cdr.car,
+            			ar.get(Integer.parseInt(result2[1])).car, ar.get(Integer.parseInt(result2[1])).cdr.car,
+            			ar.get(i5).car, ar.get(i5).cdr.car)
+        		);
+            	countOfQuestion =
+            			countOfQuestion
+            			+linearEquation(
+                    			ar.get(Integer.parseInt(result2[0])).car, ar.get(Integer.parseInt(result2[0])).cdr.car,
+                    			ar.get(Integer.parseInt(result2[1])).car, ar.get(Integer.parseInt(result2[1])).cdr.car,
+                    			ar.get(i5).car, ar.get(i5).cdr.car);
+            		//もしlinearEquationの返り血が３つ以上の場合、ノーカウント。２の場合、カウント。
+            		//finalAnswer++;
+            	}
+        	if( countOfQuestion==2){
+        		finalAnswer++;
+        		}
+        	}
+    System.out.println("finalAnswer: "+finalAnswer);
+    }
+	//linearEquation(ar.get(result2[0]).car, lattice, lattice, lattice, lattice, lattice) ;
+
 //main終わり
 
 
 	//配列をカンマ区切りの文字列に
 	public	Integer[] combining(Integer lat){
+		System.out.println("\r\n配列をカンマ区切りの文字列に実施");
 		Integer[] ary = new Integer[lat];
-		//StringBuilder builder = new StringBuilder();
 
 		for(int d =0; d<lat;d++){
 			ary[d]=d;
-			//builder.append(ary[d]).append(",");
 		}
-		//Integer result = builder.substring(0,builder.length()-1);
-		//return result;
 		return ary;
 	}
 
 
-//組み合わせのアルゴリズム（nCr）
-	public static Set<ArrayList<Integer>> getCombination(ArrayList<Integer> n, Integer r){
-		Set<ArrayList<Integer>> ans = new HashSet<ArrayList<Integer>>();
-		combination(n,r,ans);
-		for(ArrayList<Integer>e:ans){
-
-			//あとで消す
-			System.out.println("組み合わせを表示");
-
-			System.out.println(e.toString());
-			System.out.println(e.get(0));
-			System.out.println(e.get(1));
-		}
-		System.out.println("length:  "+ans.size());
-		return ans;
-		}
-
-	private static void combination(ArrayList<Integer> n, Integer r,Set<ArrayList<Integer>> ans){
-		if (n.size()==r){
-			ans.add(n);
-			return;
-		}
-		for(int i = 0;i<n.size();i++){
-			ArrayList<Integer> N = new ArrayList<Integer>();
-			N.addAll(n);
-			N.remove(i);
-			combination(N, r, ans);
-			}
-		}
 
 	//格子の中に、何個の直線があるか調べる関数
-	private static int linearEquation(){
-		return 0;
 
-	}
-
-
-
+	private static int linearEquation(int x1,int y1,int x2,int y2, int x,int y){
+		int PointOfIntersection = 0;
+		if((x2-x1)*(y-y1)==(y2-y1)*(x-x1)){
+			PointOfIntersection++;
+			}
+		return PointOfIntersection;
+		}
 
 	//x,y座標の実現のため、タプルを実装
 	  public static class Pair<A, B> {
@@ -139,6 +153,30 @@ public class Combination {
 	        public static <A, B> Tuple2<A, B> of(A a, B b) {return new Tuple2<A, B>(a, b);}
 	    }
 
+	    //組み合わせ２
+	    private enum Operation {add, remove};
+	    private static<T> Set<T> apply(Set<T> src, Operation operation, T item) {
+	        Set<T> result = new HashSet<T>(src);
+	        if (Operation.add == operation) result.add(item);
+	        if (Operation.remove == operation) result.remove(item);
+	        return result;
+	    }
+
+	    private static<T> Set<Set<T>> _combination(Set<T> selected, int depth, Set<T> pool) {
+	        if (depth == 0) return apply(new HashSet<>(), Operation.add, selected); // 選択済みのもののみ要素に持つ集合をかえす
+	        Set<Set<T>> result = new HashSet<Set<T>>();
+	        for (T item : pool) {
+	            result.addAll(
+	                    _combination(
+	                            apply(selected, Operation.add, item), // item を選択済みに変更
+	                            depth - 1,
+	                            apply(pool, Operation.remove, item)  // pool の中からは取り除く
+	                    )
+	            );
+	        }
+	        return result;
+	    }
+	    public static<T> Set<Set<T>> combination2(Set<T> src, int k) {return _combination(new HashSet<T>(), k, src);}
 
 }
 
